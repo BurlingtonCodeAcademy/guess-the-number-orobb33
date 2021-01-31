d0eba9cbd20e71abff1ca1c097b761274aef3e72
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-01-31 16:44:23 -0500

    reversegame1.js

--- reversegame1.js
+++ reversegame1.js
@@ -6,82 +6,68 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
-
+//always 1-100
 let min = 1;
 let max = 100;
 
-
-
-// function getSmartInt(min, max) {
-//     let range = Math.floor((min + max) / 2);
-//     return range;
-// }
-
-// function getRandomIntInclusive(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-//   }
-
+//random compguess generator
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 console.log(
-  "Let's play a game where I (the computer) make up a number and you (the supposed human) try to guess it. \n I will think of a number between 1-100."
+  "Let's play a game where I (the computer) make up a number and you (the supposed human) try to guess it. \n I will think of a number between 1-100.\n"
 );
 
 async function start() {
-    let firstA = await ask(
-      "Do you think you can handle that? Please use Y for yes and N for no."
-    );
-    firstA = trim(firstA);
-  
-    if (firstA === "Y") {
-      console.log("Welcome, let's proceed!\n");
+  let firstA = await ask(
+    "Do you think you can handle that? Please use Y for yes and N for no.\n"
+  );
+  firstA = trim(firstA);
+
+  if (firstA === "Y") {
+    console.log("Welcome, let's proceed!\n");
+  }
+  if (firstA === "N") {
+    console.log("Seems about right... goodbye");
+    process.exit();
+  }
+//compGen is random generated number
+  let compGen = getRandomInt(min, max);
+  compGen = +compGen;
+  //console.log(+compGen);
+//insures it is number
+  let userGuess = await ask("What is your first guess?\n");
+  userGuess = +userGuess;
+  //stores number and makes sure its number
+
+  //while (userGuess !== compGen) {
+  while (userGuess !== compGen) {
+    //maybe let / await ask function
+
+    if (userGuess < compGen) {
+      userGuess = await ask(
+        "Incorrect, the number is higher. Please guess again.\n"
+      );
+      //console.log (userGuess)
     }
-    if (firstA === "N") {
-      console.log("Seems about right... goodbye");
+    if (userGuess > compGen) {
+      userGuess = await ask(
+        "Incorrect, the number is lower. Please guess again.\n"
+      );
+      //console.log (userGuess)
+    } else if (userGuess == compGen) {
+      console.log("Congratulations! You have proved yourself...for now");
       process.exit();
     }
-
-let compGen = getRandomInt(min,max)
-compGen = +compGen
-console.log(+compGen)
-
-let userGuess = await ask ("What is your first guess?")
-    userGuess = +userGuess
-
-//while (userGuess !== compGen) { 
-while (userGuess !== compGen) {
-if (userGuess < compGen) {
-userGuess = await ask ("Incorrect, the number is higher. Please guess again.\n")
-console.log (userGuess)
-}
-if (userGuess > compGen) {
-userGuess= await ask   ("Incorrect, the number is lower. Please guess again.\n")
-console.log (userGuess)
-}
-}
-
-//WORKING UNTIL WIN
-else (userGuess === compGen) {
-    console.log ("Congratulations! You have proved yourself...for now")
-    process.exit()
-}
+  }
 }
 start();
 
 //trim funciton
 function trim(word) {
-    let trimmy = word.trim().toUpperCase();
-    return trimmy;
-  }
-
-// this will be the random number the computer saves as its number for user to guess
-// function getSmartIntR(min, max) {
-//         let range = Math.floor((min + max) / 2);
-//         return range;
-//     }
+  let trimmy = word.trim().toUpperCase();
+  return trimmy;
+}
